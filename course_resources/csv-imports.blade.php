766f6596ead0516af89927b2711f09285ea47490
Alex Garrett-Smith <alex@codecourse.com>
2023-11-27 22:05:32 +0000

    All code

---
+++ course_resources/csv-imports.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <!-- For each import -->
+        <div class="p-6">
+            <div>
+                <h2 class="font-medium">Importing [filename]</h2>
+                <span class="text-gray-700 text-sm">Imported x/y rows</span>
+                <div class="w-full bg-indigo-100 rounded">
+                    <div class="mt-2 w-full bg-indigo-500 rounded h-2"></div>
+                </div>
+            </div>
+        </div>
+    <!-- End for each import -->
+</div>
